dbb0937fc4d8588616b61f57b53cd141a41f166d
Ricardo <fankashai@gmail.com>
2016-10-05 23:05:28 -0600

    Algoritmos
    Algoritmos de backtracking, divide and conquer y programacion dinamica

---
+++ Debug/src/Main.d
@@ -0,0 +1,17 @@
+src/Main.o: ../src/Main.cpp \
+ ../src/ProgramacionDinamica/FuncionAckermann.h \
+ ../src/ProgramacionDinamica/Fibonacci.h ../src/Backtracking/Nqueen.h \
+ ../src/Backtracking/RatInMaze.h ../src/DivideAndConquer/QuickSort.h \
+ ../src/DivideAndConquer/Karatsuba.h
+
+../src/ProgramacionDinamica/FuncionAckermann.h:
+
+../src/ProgramacionDinamica/Fibonacci.h:
+
+../src/Backtracking/Nqueen.h:
+
+../src/Backtracking/RatInMaze.h:
+
+../src/DivideAndConquer/QuickSort.h:
+
+../src/DivideAndConquer/Karatsuba.h:
